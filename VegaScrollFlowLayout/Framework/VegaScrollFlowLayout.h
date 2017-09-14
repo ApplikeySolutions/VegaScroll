c6bc01ae87ac60841e30b95943782132f29ac978
Guilherme Rambo <insidegui@gmail.com>
2017-09-14 11:39:27 -0300

    Added framework project for Carthage support

---
+++ VegaScrollFlowLayout/Framework/VegaScrollFlowLayout.h
@@ -0,0 +1,19 @@
+//
+//  VegaScrollFlowLayout.h
+//  VegaScrollFlowLayout
+//
+//  Created by Guilherme Rambo on 14/09/17.
+//  Copyright © 2017 Guilherme Rambo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VegaScrollFlowLayout.
+FOUNDATION_EXPORT double VegaScrollFlowLayoutVersionNumber;
+
+//! Project version string for VegaScrollFlowLayout.
+FOUNDATION_EXPORT const unsigned char VegaScrollFlowLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VegaScrollFlowLayout/PublicHeader.h>
+
+
