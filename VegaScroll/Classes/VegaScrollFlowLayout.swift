a719ac50de0def786534e57b71c9802b5be35a1e
emrcftci <emreciftci541@gmail.com>
2020-05-18 12:09:19 +0300

    refactor(*): Upgrade Swift version & Refactor
     - Fix indentations
    
     - Change guard to if let;
        - guard is an early return statement so we should use if let in `prepare()`
    
     - Upgrade Swift version to Swift 5.0
    
     - Enable Base Internationalization

--- VegaScroll/Classes/VegaScrollFlowLayout.swift
+++ VegaScroll/Classes/VegaScrollFlowLayout.swift
@@ -51,21 +51,20 @@ open class VegaScrollFlowLayout: UICollectionViewFlowLayout {
         super.prepare()
         guard let collectionView = collectionView else { return }
 		
-		// expand the visible rect slightly to avoid flickering when scrolling quickly
-		let expandBy: CGFloat = -100
+        // expand the visible rect slightly to avoid flickering when scrolling quickly
+        let expandBy: CGFloat = -100
         let visibleRect = CGRect(origin: collectionView.bounds.origin,
                                  size: collectionView.frame.size).insetBy(dx: 0, dy: expandBy)
-        
-        guard let visibleItems = super.layoutAttributesForElements(in: visibleRect) else { return }
-        let indexPathsInVisibleRect = Set(visibleItems.map{ $0.indexPath })
-        
-        removeNoLongerVisibleBehaviors(indexPathsInVisibleRect: indexPathsInVisibleRect)
-        
-        let newlyVisibleItems = visibleItems.filter { item in
-            return !visibleIndexPaths.contains(item.indexPath)
+
+        if let visibleItems = super.layoutAttributesForElements(in: visibleRect) {
+
+            let indexPathsInVisibleRect = Set(visibleItems.map { $0.indexPath })
+            removeNoLongerVisibleBehaviors(indexPathsInVisibleRect: indexPathsInVisibleRect)
+
+            let newlyVisibleItems = visibleItems.filter { !visibleIndexPaths.contains($0.indexPath) }
+
+            addBehaviors(for: newlyVisibleItems)
         }
-        
-        addBehaviors(for: newlyVisibleItems)
     }
     
     open override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint,
@@ -85,28 +84,25 @@ open class VegaScrollFlowLayout: UICollectionViewFlowLayout {
     override open func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
         guard let collectionView = collectionView else { return nil }
         let dynamicItems = dynamicAnimator.items(in: rect) as? [UICollectionViewLayoutAttributes]
+
         dynamicItems?.forEach { item in
-			let convertedY = item.center.y - collectionView.contentOffset.y	- sectionInset.top
-			item.zIndex = item.indexPath.row
-			transformItemIfNeeded(y: convertedY, item: item)
+            let convertedY = item.center.y - collectionView.contentOffset.y	- sectionInset.top
+			      item.zIndex = item.indexPath.row
+			      transformItemIfNeeded(y: convertedY, item: item)
         }
         return dynamicItems
     }
 	
-	private func transformItemIfNeeded(y: CGFloat, item: UICollectionViewLayoutAttributes) {
-		guard itemSize.height > 0, y < itemSize.height * 0.5 else {
-			return
-		}
+    private func transformItemIfNeeded(y: CGFloat, item: UICollectionViewLayoutAttributes) {
+        guard itemSize.height > 0, y < itemSize.height * 0.5 else { return }
 		
-		let scaleFactor: CGFloat = scaleDistributor(x: y)
+        let scaleFactor: CGFloat = scaleDistributor(x: y)
+        let yDelta = getYDelta(y: y)
 		
-		let yDelta = getYDelta(y: y)
-		
-		item.transform3D = CATransform3DTranslate(transformIdentity, 0, yDelta, 0)
-		item.transform3D = CATransform3DScale(item.transform3D, scaleFactor, scaleFactor, scaleFactor)
-		item.alpha = alphaDistributor(x: y)
-	
-	}
+        item.transform3D = CATransform3DTranslate(transformIdentity, 0, yDelta, 0)
+        item.transform3D = CATransform3DScale(item.transform3D, scaleFactor, scaleFactor, scaleFactor)
+        item.alpha = alphaDistributor(x: y)
+    }
 	
     override open func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
         return dynamicAnimator.layoutAttributesForCell(at: indexPath)!
@@ -119,7 +115,7 @@ open class VegaScrollFlowLayout: UICollectionViewFlowLayout {
         
         let touchLocation = collectionView!.panGestureRecognizer.location(in: collectionView)
         
-        dynamicAnimator.behaviors.flatMap { $0 as? UIAttachmentBehavior }.forEach { behavior in
+        dynamicAnimator.behaviors.compactMap { $0 as? UIAttachmentBehavior }.forEach { behavior in
             let attrs = behavior.items.first as! UICollectionViewLayoutAttributes
             attrs.center = getUpdatedBehaviorItemCenter(behavior: behavior, touchLocation: touchLocation)
             self.dynamicAnimator.updateItem(usingCurrentState: attrs)
@@ -168,8 +164,8 @@ open class VegaScrollFlowLayout: UICollectionViewFlowLayout {
     
     private func getUpdatedBehaviorItemCenter(behavior: UIAttachmentBehavior,
                                               touchLocation: CGPoint) -> CGPoint {
-        let yDistanceFromTouch = fabs(touchLocation.y - behavior.anchorPoint.y)
-        let xDistanceFromTouch = fabs(touchLocation.x - behavior.anchorPoint.x)
+        let yDistanceFromTouch = abs(touchLocation.y - behavior.anchorPoint.y)
+        let xDistanceFromTouch = abs(touchLocation.x - behavior.anchorPoint.x)
         let scrollResistance = (yDistanceFromTouch + xDistanceFromTouch) / (springHardness * 100)
         
         let attrs = behavior.items.first as! UICollectionViewLayoutAttributes
@@ -191,24 +187,22 @@ open class VegaScrollFlowLayout: UICollectionViewFlowLayout {
      - parameter xOrigin: x coordinate of the function origin.
      */
     private func distributor(x: CGFloat, threshold: CGFloat, xOrigin: CGFloat) -> CGFloat {
-		guard threshold > xOrigin else {
-			return 1
-		}
+        guard threshold > xOrigin else { return 1 }
         var arg = (x - xOrigin)/(threshold - xOrigin)
         arg = arg <= 0 ? 0 : arg
         let y = sqrt(arg)
         return y > 1 ? 1 : y
     }
 	
-	private func scaleDistributor(x: CGFloat) -> CGFloat {
-		return distributor(x: x, threshold: itemSize.height * 0.5, xOrigin: -itemSize.height * 5)
+    private func scaleDistributor(x: CGFloat) -> CGFloat {
+        return distributor(x: x, threshold: itemSize.height * 0.5, xOrigin: -itemSize.height * 5)
     }
     
     private func alphaDistributor(x: CGFloat) -> CGFloat {
-		return distributor(x: x, threshold: itemSize.height * 0.5, xOrigin: -itemSize.height)
+        return distributor(x: x, threshold: itemSize.height * 0.5, xOrigin: -itemSize.height)
     }
 	
-	private func getYDelta(y: CGFloat) -> CGFloat {
-		return itemSize.height * 0.5 - y
-	}
+    private func getYDelta(y: CGFloat) -> CGFloat {
+        return itemSize.height * 0.5 - y
+    }
 }
